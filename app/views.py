b14a8ea9e8bbc6b515c50ffb8657ca6e624c0ce4
Tzahi Vidas <tzahi.vidas@gmail.com>
2015-07-03 16:23:00 -0400

    Add reactjs to front end

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import Flask, render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+	return render_template('hello.html')
