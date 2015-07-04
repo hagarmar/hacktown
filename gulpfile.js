b627709e32efa4d4c9c7c1a940527bb33238783b
Tzahi Vidas <tzahi.vidas@gmail.com>
2015-07-04 13:04:46 -0400

    gulp package

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// requirements
+
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify'),
+    size = require('gulp-size'),
+    clean = require('gulp-clean');
+
+
+// tasks
+
+gulp.task('transform', function () {
+  // add task
+});
+
+gulp.task('clean', function () {
+  // add task
+});
+
+gulp.task('default', function() {
+  console.log("hello!")
+});
